50ef47a2d2a80ca6edd721174c70a4b8da8c845f
Ahmedm2002 <ahmedmujtaba0159@gmail.com>
2026-03-28 10:14:21 +0500

    Implemented signalling with type interfaces

--- src/components/singalling.ts
+++ src/components/singalling.ts
@@ -2,11 +2,14 @@ import { app } from "../app.js";
 import { Server, Socket } from "socket.io";
 import logger from "../utils/logger/logger.js";
 import { createServer } from "node:http";
+import type {
+  WebRTCOfferPayload,
+  WebRTCAnswerPayload,
+  WebRTCIceCandidatePayload,
+} from "../interfaces/webrtc.connections.models.js";
 
 const server = createServer(app);
-const io = new Server(server, {
-  serveClient: false,
-});
+const io = new Server(server);
 
 const emailToSocketMap: Map<string, { socketId: string; name: string }> =
   new Map();
@@ -14,7 +17,7 @@ const emailToSocketMap: Map<string, { socketId: string; name: string }> =
 io.on("connection", (socket: Socket) => {
   logger.info({ socketId: socket.id }, "New client connected");
 
-  socket.on("user:register", (email: string, name: string) => {
+  socket.on("register", ({ email, name }) => {
     if (!name || !email) {
       logger.warn({ socketId: socket.id }, "Invalid registration data");
       return;
@@ -26,8 +29,59 @@ io.on("connection", (socket: Socket) => {
     );
   });
 
+  socket.on("offer", (data: WebRTCOfferPayload) => {
+    const targetUser = emailToSocketMap.get(data.to);
+    if (!targetUser) {
+      socket.emit("user-status", { isOnline: false, message: "user offline" });
+      return;
+    }
+    socket.to(targetUser.socketId).emit("offer", {
+      offer: data.offer,
+      from: data.from,
+    });
+  });
+
+  socket.on("answer", (data: WebRTCAnswerPayload) => {
+    const targetUser = emailToSocketMap.get(data.to);
+    if (!targetUser) {
+      socket.emit("user-status", { isOnline: false, message: "user offline" });
+      return;
+    }
+    socket.to(targetUser.socketId).emit("answer", {
+      answer: data.answer,
+      from: data.from,
+    });
+  });
+
+  socket.on("ice-candidate", (data: WebRTCIceCandidatePayload) => {
+    const targetUser = emailToSocketMap.get(data.to);
+    if (!targetUser) {
+      socket.emit("user-status", { isOnline: false, message: "user offline" });
+      return;
+    }
+    socket.to(targetUser.socketId).emit("ice-candidate", {
+      candidate: data.candidate,
+      from: data.from,
+    });
+  });
+
   socket.on("disconnect", () => {
+    removeEmailFromMap(socket.id);
     logger.info({ socketId: socket.id }, "Client disconnected");
   });
 });
+
 export default server;
+
+function removeEmailFromMap(id: string) {
+  if (!id) return;
+  const entry = getEmailBySocketId(id);
+  if (!entry) return;
+  emailToSocketMap.delete(entry);
+}
+
+function getEmailBySocketId(id: string): string | null {
+  const sockets = Array.from(emailToSocketMap.entries());
+  const entry = sockets.find(([_, value]) => value.socketId === id);
+  return entry ? entry[0] : null;
+}
---
+++ src/interfaces/webrtc.connections.models.ts
@@ -0,0 +1,17 @@
+export interface WebRTCOfferPayload {
+  from: string;
+  to: string;
+  offer: any;
+}
+
+export interface WebRTCAnswerPayload {
+  from: string;
+  to: string;
+  answer: any;
+}
+
+export interface WebRTCIceCandidatePayload {
+  from: string;
+  to: string;
+  candidate: any;
+}
