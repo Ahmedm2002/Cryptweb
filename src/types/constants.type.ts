a67ad3dacb6c7a0ad3973742e107a9f5052269d4
Ahmed Mujtaba <ahmedmujtaba0159@gmail.com>
2026-06-08 23:09:59 +0500

    domain added in cookies options

--- src/types/constants.type.ts
+++ src/types/constants.type.ts
@@ -6,7 +6,7 @@ type CONSTS = {
     path: string;
     sameSite: "lax" | "none" | "strict" | boolean;
     maxAge: number;
-    Domain: string | undefined;
+    Domain: string;
   };
   authCookieOpts: {
     httpOnly: boolean;
@@ -14,7 +14,7 @@ type CONSTS = {
     path: string;
     sameSite: "lax" | "none" | "strict" | boolean;
     maxAge: number;
-    Domain: string | undefined;
+    Domain: string;
   };
   OTP_EXPIRY_MS: number;
 };
